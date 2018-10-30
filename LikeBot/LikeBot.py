be2ac2ae07f9d49bfa80c14fc831905ad6cc8a53
Darren Jones <darren.jones@kohls.com>
2018-10-30 14:47:37 -0500

    Restructure

--- LikeBot/LikeBot.py
+++ LikeBot/LikeBot.py
@@ -9,6 +9,7 @@ import time
 import re
 import logging
 import random
+import requests
 from slackclient import SlackClient
 from database import LikeBotDatabase
 from config import SLACK_BOT_TOKEN
@@ -23,28 +24,6 @@ db = None
 # Constants
 RTM_READ_DELAY = 1 # 1-second delay RTM read
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
-HELP_TEXT = """
-Basic Commands:
-        *help* - find a list of commands
-        *hello* - say hi to me
-        *about* - learn about this bot
-User Commands:
-        *register* _*[first_name]*_ _*[last_name]*_ - register your name and establish like count
-        *me* - see if you are registered, your name and your like count
-        *likes* - see your current like count
-        *id* - see your Slack user id
-Feature Commands:
-        *coinflip/flip* _*[heads/tails]*_ - flip a coin
-        *scoreboard* - see the like count of all users
-API Commands:
-        *bitcoin/btc* - get the current Bitcoin price in USD
-        *stock* _*[ticker]*_ - get the current stock price for a given ticker in USD
-        *kss* - get the current stock price of Kohl's (KSS)
-        *dog* - random dog picture
-        *trump* - random quote from Donald Trump
-  
-"""
-
 
 def parse_bot_commands(slack_events):
     for event in slack_events:
@@ -68,71 +47,104 @@ def handle_command(command, channel, sender_id, text):
     # Finds and executes given command, filling in response
     command = command.lower()
     msg = str(" ".join(text.lower().split(" ")[1:]))
+    commands = msg.split(" ")
+    main_commands = {'like':1,'dislike':-1,'love':2,'hate': -2}
     response = None
     attachments = None
 
     print(text)
 
-    commands = msg.split(" ")
-    main_commands = {'like':1,'dislike':-1,'love':2,'hate': -2}
-
     if commands[0] in main_commands:
-        name = " ".join(commands[1:])
-        if db.getName(sender_id) == name:
-            message(channel, "you cannot " + commands[0] + " yourself, silly willy")
+        like(channel, sender_id, commands, msg, main_commands)
+    elif msg == 'scoreboard' or msg == 'anti-scoreboard':
+        scoreboard(channel, msg)
+    elif commands[0] == 'score':
+        score(channel, sender_id, commands)
+    elif commands[0] == 'fight':
+        fight(channel, sender_id, commands)    
+    elif commands[0] == 'wager':
+        wager(channel, sender_id, commands)
+    elif msg == 'help':
+        helper(channel)
+    else:
+        message(channel, requests.get("https://api.chew.pro/trbmb"))
+    
+def like(channel, sender_id, commands, msg, main_commands):
+
+    name = " ".join(commands[1:])
+    if db.getName(sender_id) == name:
+        message(channel, "you cannot " + commands[0] + " yourself, silly willy")
+    else:
+        name = " ".join(commands[1:]).lower()
+        thing = db.getThing(name)
+        if thing:
+            db.addLikes(thing.thing_id, main_commands[commands[0]])
+            likes = str(thing.like_bal + main_commands[commands[0]])
+            message(channel, db.getName(sender_id) + " " + commands[0] + "d '" + name + "' and added " + str(main_commands[commands[0]])
+                    + " like(s). '" + name + "' now has " + likes + " like(s)!")
         else:
-            name = " ".join(commands[1:]).lower()
+            db.createThing(name)
             thing = db.getThing(name)
             if thing:
                 db.addLikes(thing.thing_id, main_commands[commands[0]])
                 likes = str(thing.like_bal + main_commands[commands[0]])
                 message(channel, db.getName(sender_id) + " " + commands[0] + "d '" + name + "' and added " + str(main_commands[commands[0]])
                         + " like(s). '" + name + "' now has " + likes + " like(s)!")
-            else:
-                db.createThing(name);
-                thing = db.getThing(name)
-                if thing:
-                    db.addLikes(thing.thing_id, main_commands[commands[0]])
-                    likes = str(thing.like_bal + main_commands[commands[0]])
-                    message(channel, db.getName(sender_id) + " " + commands[0] + "d '" + name + "' and added " + str(main_commands[commands[0]])
-                            + " like(s). '" + name + "' now has " + likes + " like(s)!")
-    elif msg == 'scoreboard' or msg == 'anti-scoreboard':
-        display = 'DESC' if msg == 'scoreboard' else 'ASC'
-        thing_list = db.scoreboard(display);
-        scoreboard = []
-        for thing in thing_list:
-            scoreboard.append(thing.name + ": " + str(thing.like_bal))
-        message(channel, "  \n".join(scoreboard))
-    elif commands[0] == 'score':
-        name = " ".join(commands[1:])
-        thing = db.getThing(name)
-        if thing:
+    
+def scoreboard(channel, msg):
+
+    display = 'DESC' if msg == 'scoreboard' else 'ASC'
+    thing_list = db.scoreboard(display)
+    scoreboard = []
+    for thing in thing_list:
+        scoreboard.append(thing.name + ": " + str(thing.like_bal))
+    message(channel, "  \n".join(scoreboard))
+
+def score(channel, sender_id, commands):
+    
+    name = " ".join(commands[1:])
+    thing = db.getThing(name)
+    if thing:
+        if thing.name == db.getName(sender_id):
+            message(channel, "u haz " + str(thing.like_bal) + " likes")
+        else:
             message(channel, "they haz " + str(thing.like_bal) + " likes")
-        else:
-            message(channel, "idk who that is")
-    elif commands[0] == 'fight':
-        fighter = db.getThing(db.getName(sender_id))
-        fightee = db.getThing(" ".join(commands[1:]))
-
-        if fighter and fightee:
-            if db.getName(sender_id) == fightee.name:
-                message(channel, "you cannot fight yourself, silly willy")
-                return
-
-            if random.random() >= .5:
-                db.addLikes(fighter.thing_id, 2)
-                db.addLikes(fightee.thing_id, -2)
-                message(channel, fighter.name + " has won the fight and has stole 2 likes from " + fightee.name)
-            else:
-                db.addLikes(fighter.thing_id, -2)
-                db.addLikes(fightee.thing_id, 2)
-                message(channel, fightee.name + " has won the fight and has stole 2 likes from " + fighter.name)
-        else:
-            message(channel, "idk who u is trying to fight")
-    elif commands[0] == 'wager':
-        message(channel, "feature coming soon!")
     else:
-        message(channel, response)
+        message(channel, "idk who that is")
+
+def fight(channel, sender_id, commands):            
+    
+    fighter = db.getThing(db.getName(sender_id))
+    fightee = db.getThing(" ".join(commands[1:]))
+
+    if fighter and fightee:
+        if db.getName(sender_id) == fightee.name:
+            message(channel, "you cannot fight yourself, silly willy")
+            return
+
+        if random.random() >= .5:
+            db.addLikes(fighter.thing_id, 2)
+            db.addLikes(fightee.thing_id, -2)
+            message(channel, fighter.name + " has won the fight and has stole 2 likes from " + fightee.name)
+        else:
+            db.addLikes(fighter.thing_id, -2)
+            db.addLikes(fightee.thing_id, 2)
+            message(channel, fightee.name + " has won the fight and has stole 2 likes from " + fighter.name)
+    else:
+        message(channel, "idk who u is trying to fight")
+
+def wager(channel, sender_id, commands):            
+    
+    message(channel, "feature coming soon!")
+
+def helper(channel):
+
+    message(channel, "You want to know how to use me, eh?  \n  \n" +
+            "* '[Like, Dislike, Love, Hate] {thing}': This command add or subtracts likes from the designated {thing}.  \n" +
+            "* 'Fight {person}': This command initiates a fight between you and the {person}. The winner gains two likes while the loser loses two.  \n" +
+            "* 'Wager {person} {amount}': This command is for betting your likes on fights. You will gain/lose likes based on the {amount} chosen and the {person} who wins.  \n" +
+            "* 'Score {thing}': This command displays the current like count of the designated {thing}.  \n" +
+            "* '[Scoreboard, Anti-scoreboard]': This command displays the top 10 and bottom 10 things respectively in terms of likes.  \n")
 
 def message(channel, response):
     # Sends response back to channel.
